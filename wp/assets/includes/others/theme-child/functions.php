9bbb0d5e46d174dfe570f21f6818eb19470f559f
Sammy VeryCreative <sammyverycreative@gmail.com>
2020-02-03 15:52:43 +0200

    Default WP Starter

--- wp/assets/includes/others/theme-child/functions.php
+++ wp/assets/includes/others/theme-child/functions.php
@@ -1,11 +1,13 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+/*
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+*/
 
-/*function my_theme_enqueue_styles() {
+function my_theme_enqueue_styles() {
 
     $parent_style = 'parent-style'; // theme-style
 
@@ -16,5 +18,5 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );*/
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
--- wp/assets/includes/others/theme-child/script.js
+++ wp/assets/includes/others/theme-child/script.js
@@ -1,9 +1,6 @@
-/*
-Theme Name: Theme_Name Child
-Theme URI: Theme_URL
-Author: VeryCreative
-Author URI: http://www.verycreative.ro
-Version: 1.0
-*/
-
 // CHILD THEME SCRIPT
+jQuery(document).ready(function($) {
+
+	
+
+});
